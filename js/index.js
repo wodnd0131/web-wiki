4d113aaa4af4880d4a7d4520f667dc8adfa8fbad
황재웅 <wodnd0131@naver.com>
2025-02-27 20:38:17 +0900

    faet: misson 요구사항 구현

--- js/index.js
+++ js/index.js
@@ -17,3 +17,33 @@ console.log(
 );
 
 console.log("알맞은 스크립트를 작성하세요");
+document.addEventListener('DOMContentLoaded', function() {
+    const submitBtn = document.getElementById('submitComment');
+    if (submitBtn) {
+        submitBtn.addEventListener('click', addComment);
+    }
+});
+function addComment(event) {
+    event.preventDefault();
+
+    const commentText = document.getElementById('commentText').value;
+
+    const newComment = document.createElement('li');
+    newComment.innerHTML = `
+        <div class="comment-item">
+          <div class="comment-author">
+            <img src="./images/comment-author-icon.png  " alt="사용자 프로필 이미지" />
+            <span>방문자</span>
+          </div>
+          <div class="comment-content">
+            ${commentText}
+          </div>
+        </div>
+      `;
+
+    document.getElementById('commentList').appendChild(newComment);
+
+    document.getElementById('commentForm').reset();
+
+    alert('댓글이 등록되었습니다.');
+}
